c9f20d1b428985d0700e28bf4248f0017ba93508
Tom Spettigue <apickle@gmail.com>
2018-04-16 10:55:15 -0600

    saved library creation request as variable

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+const baseURL = "https://floating-woodland-64068.herokuapp.com/";
+const libraryID = 154;
+
+let createLibraryRequest = $.ajax({type:"POST",url:`${baseURL}libraries`,data:{library:{name:"My Secret Library"}}});
+
 // Shorthand for $( document ).ready()
 $(() => {
   console.log( "ready!" );
